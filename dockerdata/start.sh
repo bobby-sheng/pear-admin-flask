5c9d8f4c36102cc0b300288a0b73d85363cd5087
六翼羊王 <1993333339@qq.com>
2023-04-13 01:20:57 +0800

    修改docker-compose部署方式

--- dockerdata/start.sh
+++ dockerdata/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 5 #等MySQL启动
 flask db init && flask db migrate && flask db upgrade && flask admin init
-exec gunicorn -c gunicorn.conf.py "applications:create_app()"
+exec gunicorn -c gunicorn.conf.py "applications:create_app() --preload"
 while true; do
     echo '发生错误'
     sleep 100
