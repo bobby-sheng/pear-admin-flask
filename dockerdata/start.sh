392301bec520f9d49bcbde75dbe0a8d60c55266d
六翼羊王 <1993333339@qq.com>
2023-04-17 07:26:16 +0000

    update dockerdata/start.sh.
    Signed-off-by: 六翼羊王 <1993333339@qq.com>

--- dockerdata/start.sh
+++ dockerdata/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-sleep 5
+sleep 10
 rm -rf migrations
-flask db init && flask db migrate && flask db upgrade && flask admin init
+flask db init &
+flask db migrate && flask db upgrade && flask admin init
 exec gunicorn -c gunicorn.conf.py "applications:create_app()"
 while true; do
     echo '发生错误'
