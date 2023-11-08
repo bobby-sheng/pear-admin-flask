ffcb938b9cff5405be2d6ead03a4cae132f06cdb
张圣波 <shenbo.zhang@sky-cloud.net>
2023-11-08 16:41:52 +0800

    [fix]修改单引号字段转换yaml

--- applications/view/system/json_to_yaml.py
+++ applications/view/system/json_to_yaml.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request
 from applications.common.utils.rights import authorize
 import json
 import yaml
+import jsonpath
 
 bp = Blueprint('json_to_yaml', __name__, url_prefix='/json_to_yaml')
 
@@ -27,5 +28,20 @@ def convert():
             data = eval(data)
         yaml_data = yaml.dump(data, default_flow_style=False, sort_keys=False, allow_unicode=True)
         return yaml_data
+    except Exception as e:
+        return str(e)
+
+
+@bp.route('/jsonpath_to', methods=['POST'])
+@authorize("system:json_to_yaml:main")
+def jsonpath_to():
+    json_data = request.form['json']
+    json_path = request.form['json_path']
+    try:
+        data = json.loads(json_data)
+        if isinstance(data, str):
+            data = eval(data)
+        jsonpata_data = jsonpath.jsonpath(data, json_path)
+        return str(jsonpata_data)
     except Exception as e:
         return str(e)
